7296cb7b465ca2e221f99483e169183387930dce
byytg <bachir.55.lebboukh@gmail.com>
2026-07-30 23:30:01 +0100

    Refactor and clean up script.js

--- script.js
+++ script.js
@@ -46,19 +46,6 @@ yesBtn.onclick = () => {
     startFireworks();
 
 }
-const yesBtn = document.getElementById("yes");
-
-yesBtn.addEventListener("click", () => {
-
-    // اهتزاز لمدة 200 مللي ثانية
-    if (navigator.vibrate) {
-        navigator.vibrate(200);
-    }
-
-function vibrate(pattern = 100) {
-    if ("vibrate" in navigator) {
-        navigator.vibrate(pattern);
-    }
 
 // =========================
 // قلوب متطايرة
